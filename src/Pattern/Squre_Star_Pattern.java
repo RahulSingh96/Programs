dcb4b360878375543429f7d86a99fac3910c3c40
rs388 <rs388903@gmail.com>
2022-12-13 22:42:22 +0530

    Initial commit
    Signed-off-by: rs388 <rs388903@gmail.com>

---
+++ src/Pattern/Squre_Star_Pattern.java
@@ -0,0 +1,28 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class Squre_Star_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i =1;
+        while(i<=n){
+            int j =1;
+            while(j<=n){
+                System.out.print(" * ");
+                j = j+1;
+            }
+            System.out.println();
+            i = i+1;
+        }
+    }
+}
+
+//       6
+//        *  *  *  *  *  *
+//        *  *  *  *  *  *
+//        *  *  *  *  *  *
+//        *  *  *  *  *  *
+//        *  *  *  *  *  *
+//        *  *  *  *  *  *
