1d218edb95918422aed26444f666f46f6b954f7e
rs388 <rs388903@gmail.com>
2022-12-17 10:55:11 +0530

    Initial commit
    Signed-off-by: rs388 <rs388903@gmail.com>

--- src/Pattern/Diamond_Star_Pattern.java
+++ src/Pattern/Diamond_Star_Pattern.java
@@ -7,21 +7,21 @@ public class Diamond_Star_Pattern {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int p = 1;
-        for (int i =1; i<=n; i++){
-            for(int j =1; j<=n-i; j++){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int m =1; m<=i*2-1; m++){
+            for (int m = 1; m <= i * 2 - 1; m++) {
                 System.out.print("*");
 
             }
             System.out.println();
         }
-        for (int i =n-1; i>=1; i--){
-            for(int j =n-i; j>=1; j--){
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n - i; j >= 1; j--) {
                 System.out.print(" ");
             }
-            for (int m =i*2-1; m>=1; m--){
+            for (int m = i * 2 - 1; m >= 1; m--) {
                 System.out.print("*");
 
             }
