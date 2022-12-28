0f2b74063a02d943ee6bfec80453f209d640cf2c
rs388 <rs388903@gmail.com>
2022-12-28 19:03:00 +0530

    Initial commit
    Signed-off-by: rs388 <rs388903@gmail.com>

--- src/String/Palindrome_String.java
+++ src/String/Palindrome_String.java
@@ -1,5 +1,25 @@
 package String;
 
+import java.util.Scanner;
+
 public class Palindrome_String {
+    public static boolean reverseString(String str) {
+        String str1 = "";
+        boolean str2 = true;
+        for (int i = str.length()-1 ; i >= 0; i--) {
+            str1 = str1+str.charAt(i);
+        }
+
+        boolean com = str.equals(str1);
+        return com;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        boolean reversedString = reverseString(str);
+        System.out.println(reversedString);
+
+    }
 
 }
